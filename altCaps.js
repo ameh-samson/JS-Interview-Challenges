50d2fbac9883f79ceb8b7c05079bb7a0f83d90cd
Samson Ameh <amehsam69@gmail.com>
2024-01-12 13:27:04 +0100

    altCaps function added

---
+++ altCaps.js
@@ -0,0 +1,17 @@
+// /* Alternating Caps
+//  Write a function that takes in a string of letters
+//  and returns a sentence in which every other letter is capitalized.
+
+// Example input: "I'm so happy it's Monday"
+// Example output: "I'M So hApPy iT'S MoNdAy"
+// */
+
+function altCaps(str) {
+  let newStr = str.split("");
+  for (let i = 0; i < newStr.length; i += 2) {
+    newStr[i] = newStr[i].toUpperCase();
+  }
+  return newStr.join("");
+}
+
+console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
