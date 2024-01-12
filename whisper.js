c8a57b99ca32bb6c0c5a4cf3911e5e71dfad401a
Samson Ameh <amehsam69@gmail.com>
2024-01-12 12:49:33 +0100

    conditional return fixed

--- whisper.js
+++ whisper.js
@@ -14,7 +14,11 @@
 // */
 
 function whisper(str) {
-  return `shh... ${str.slice(0, -1).toLowerCase()}`;
+  if (str.endsWith("!")) {
+    return `shh... ${str.slice(0, -1).toLowerCase()}`;
+  } else {
+    return `shh... ${str.toLowerCase()}`;
+  }
 }
 
 console.log(whisper("PLEASE STOP SHOUTING."));
