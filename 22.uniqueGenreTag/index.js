ad3a1c3dfc11f0fb0d9c78cb03db888d4a1cf728
Samson Ameh <amehsam69@gmail.com>
2024-01-16 02:34:05 +0100

    functions added

--- 22.uniqueGenreTag/index.js
+++ 22.uniqueGenreTag/index.js
@@ -16,6 +16,50 @@ import mediaData from "./data.js";
 
 // */
 
-function getUniqueTags(data) {}
+// function getUniqueTags(data){
+//     // use map to loop through the data and get a new array of tags
+//     // flatten the tags array with .flat()
+//     const tags = data.map(podcast => podcast.tags).flat();
+//     // create a new array uniqueTags to hold the unique values
+//     const uniqueTags = [];
+//     // loop through the tags array
+
+//     tags.forEach(tag => {
+//          // is the element already in the uniqueTags arr?
+//          if(!uniqueTags.includes(tag)){
+//              uniqueTags.push(tag)
+//          }
+//     })
+
+//     return uniqueTags;
+// }
+
+// ALTERNATIVE SOLUTION
+
+// function getUniqueTags(data) {
+//   // loops through and flatten the array
+//   const dataArr = data.map((podcast) => podcast.tags).flat();
+
+//   //   removes duplicates Set()
+//   return [...new Set(dataArr)];
+// }
+
+// ALTERNATIVE SOLUTION
+
+function getUniqueTags(data) {
+  const tags = data.map((podcast) => podcast.tags).flat();
+  const uniqueTags = {};
+
+  // filter tags arr
+  return tags.filter((tag) => {
+    // look up the tag in the uniqueTags obj
+    if (!uniqueTags[tag]) {
+      uniqueTags[tag] = true;
+      // if it's not the, we have a unique item
+      // put the item in our object with a value of true
+      return true;
+    }
+  });
+}
 
 console.log(getUniqueTags(mediaData));
